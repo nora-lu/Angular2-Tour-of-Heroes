fc6bff4569df8a56fbcd3ba7ab59175ca2d8ff15
Qian Lu <desireemikan@gmail.com>
2016-11-18 12:38:35 -0500

    Add service

---
+++ app/hero.service.ts
@@ -0,0 +1,21 @@
+/**
+ * Created by qian on 11/18/16.
+ */
+import { Injectable } from '@angular/core';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+@Injectable()
+export class HeroService {
+    getHeroes(): Promise<Hero[]> {
+        return Promise.resolve(HEROES);
+    }
+
+    // simulate a slow connection
+    getHeroesSlowly(): Promise<Hero[]> {
+        return new Promise<Hero[]>(resolve =>
+            setTimeout(resolve, 2000))
+                .then(() => this.getHeroes());
+    }
+}
